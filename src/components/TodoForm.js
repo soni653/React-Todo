f1e0f707988ae28623bdc5977b34f8f99c5aedb6
soni653 <dave.family653@outlook.com>
2020-04-08 20:53:48 -0400

    done MVP

---
+++ src/components/TodoForm.js
@@ -0,0 +1,27 @@
+import React, {Component} from "react";
+class TodoForm extends Component {
+    constructor(){
+        super();
+        this.state = {
+            todo: ''
+        };
+    }
+    handleChanges = e => {
+        this.setState ({ [e.target.name]: e.target.value });
+    };
+    submitTodo = e => {
+        e.preventDefault();
+        this.props.addTodo(e, this.state.todo);
+        this.setState({todo: ""});
+    };
+    render () {
+        return (
+            <form onSubmit = {this.submitTodo}>
+                <input type="text" name="todo" 
+                onChange={this.handleChanges} />
+                <button>Add Todo</button>
+            </form>
+        );
+    }
+}
+export default TodoForm;
